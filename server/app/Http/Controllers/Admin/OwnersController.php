a4ad861a59999f147c5fa8479a257beaf37a660d
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-03-18 12:02:39 +0900

    52 一覧画面(tailblocks利用)

--- server/app/Http/Controllers/Admin/OwnersController.php
+++ server/app/Http/Controllers/Admin/OwnersController.php
@@ -17,14 +17,14 @@ class OwnersController extends Controller
 
     public function index()
     {
-        $date_now = Carbon::now();
-        $date_parse = Carbon::parse(now());
-        echo $date_now . '<br>';
-        echo $date_now->year . '<br>';
-        echo $date_parse . '<br>';
+        // $date_now = Carbon::now();
+        // $date_parse = Carbon::parse(now());
+        // echo $date_now . '<br>';
+        // echo $date_now->year . '<br>';
+        // echo $date_parse . '<br>';
 
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $e_all = Owner::all();
+        // $q_get = DB::table('owners')->select('name', 'created_at')->get();
         // $q_first = DB::table('owners')->select('name')->first();
 
         // $c_test = collect([
@@ -35,7 +35,9 @@ class OwnersController extends Controller
 
         // dd($e_all, $q_get, $q_first, $c_test);
 
-        return view('admin.owners.index', compact('e_all', 'q_get'));
+        $owners = Owner::select('name', 'email', 'created_at')->get();
+
+        return view('admin.owners.index', compact('owners'));
     }
 
     /**
