e02363bfa6f468457a5fd0b4aab33755ed89a62b
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-04-12 19:04:09 +0900

    125 商品の詳細 数量

--- server/app/Http/Controllers/User/ItemController.php
+++ server/app/Http/Controllers/User/ItemController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\Stock;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -54,7 +55,12 @@ class ItemController extends Controller
     public function show($id)
     {
         $product = Product::findOrFail($id);
+        $quantity = Stock::where('product_id', $product->id)->sum('quantity');
 
-        return view('user.show', compact('product'));
+        if ($quantity > 9) {
+            $quantity = 9;
+        }
+
+        return view('user.show', compact('product', 'quantity'));
     }
 }
