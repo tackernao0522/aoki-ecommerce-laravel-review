2bc741b3732c0aadad24a01b5652aad9a216580e
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-04-12 16:21:51 +0900

    121 Swiper.js(カルーセル) その1

--- server/resources/views/user/show.blade.php
+++ server/resources/views/user/show.blade.php
@@ -11,17 +11,39 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="md:flex md:justify-around">
                         <div class="md:w-1/2">
-                            <x-thumbnail filename="{{ $product->imageFirst->filename ?? '' }}" type="products" />
+                            <div class="swiper-container">
+                                <!-- Additional required wrapper -->
+                                <div class="swiper-wrapper">
+                                    <!-- Slides -->
+                                    <div class="swiper-slide"><img src="{{ asset('storage/products/sample1.jpg') }}"
+                                            alt=""></div>
+                                    <div class="swiper-slide">Slide 2</div>
+                                    <div class="swiper-slide">Slide 3</div>
+                                    ...
+                                </div>
+                                <!-- If we need pagination -->
+                                <div class="swiper-pagination"></div>
+
+                                <!-- If we need navigation buttons -->
+                                <div class="swiper-button-prev"></div>
+                                <div class="swiper-button-next"></div>
+
+                                <!-- If we need scrollbar -->
+                                <div class="swiper-scrollbar"></div>
+                            </div>
                         </div>
                         <div class="md:w-1/2 ml-4">
-                            <h2 class="mb-4 text-sm title-font text-gray-500 tracking-widest">{{ $product->category->name }}
+                            <h2 class="mb-4 text-sm title-font text-gray-500 tracking-widest">
+                                {{ $product->category->name }}
                             </h2>
                             <h1 class="text-gray-900 text-3xl title-font font-medium mb-4">{{ $product->name }}</h1>
                             <p class="mb-4 leading-relaxed">{{ $product->information }}</p>
                             <div class="flex justify-around items-center">
-                              <div>
-                                <span class="title-font font-medium text-2xl text-gray-900">{{ number_format($product->price) }}<span class="text-sm text-gray-700">円(税込)</span></span>
-                              </div>
+                                <div>
+                                    <span
+                                        class="title-font font-medium text-2xl text-gray-900">{{ number_format($product->price) }}<span
+                                            class="text-sm text-gray-700">円(税込)</span></span>
+                                </div>
                                 <div class="flex items-center ml-auto">
                                     <span class="mr-3">数量</span>
                                     <div class="relative">
@@ -43,4 +65,5 @@
             </div>
         </div>
     </div>
+    <script src="{{ mix('js/swiper.js') }}"></script>
 </x-app-layout>
